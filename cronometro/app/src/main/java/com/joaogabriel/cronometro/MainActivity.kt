28d9b2bbd488994641e9b225784984bad8de68a7
João Gabriel <joao.gabrielfjob@gmail.com>
2021-02-24 16:42:07 -0300

    adicionando logica do cronometro

--- cronometro/app/src/main/java/com/joaogabriel/cronometro/MainActivity.kt
+++ cronometro/app/src/main/java/com/joaogabriel/cronometro/MainActivity.kt
@@ -2,10 +2,45 @@ package com.joaogabriel.cronometro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.joaogabriel.cronometro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+        var running  = false
+        var pause: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciar.setOnClickListener { iniciarCronometro() }
+        binding.pausar.setOnClickListener { pausarCronometro() }
+        binding.zerar.setOnClickListener { zerarCronometro() }
+    }
+
+    private fun iniciarCronometro() {
+        if (!running) {
+            binding.cronometro.base = SystemClock.elapsedRealtime() - pause
+            binding.cronometro.start()
+
+            running = true
+        }
+    }
+
+    private fun pausarCronometro() {
+        if (running) {
+            binding.cronometro.stop()
+            pause = SystemClock.elapsedRealtime() - binding.cronometro.base
+
+            running = false
+        }
+    }
+
+    private fun zerarCronometro() {
+        binding.cronometro.base = SystemClock.elapsedRealtime()
+        pause = 0
     }
 }
