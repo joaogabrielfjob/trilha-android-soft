eb224ce6bf93d877381bf21edba9a0a09ef9be70
João Gabriel <joao.gabrielfjob@gmail.com>
2021-02-24 18:49:26 -0300

    adicionando logica da calculadora imc

--- calculadora-imc/app/src/main/java/com/joaogabriel/calculadoraimc/MainActivity.kt
+++ calculadora-imc/app/src/main/java/com/joaogabriel/calculadoraimc/MainActivity.kt
@@ -2,6 +2,7 @@ package com.joaogabriel.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.joaogabriel.calculadoraimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,23 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.botaoCalcularImc.setOnClickListener { calcularImc() }
+        binding.botaoLimparCampos.setOnClickListener { limparCampos() }
+    }
+
+    private fun calcularImc() {
+        val peso = binding.campoPeso.text.toString().toInt()
+        val altura = binding.campoAltura.text.toString().toDouble()
+        val imc = peso / (altura * altura)
+
+        val resultado = getString(R.string.imc, imc)
+        binding.campoResultado.text = resultado
+        binding.campoResultado.visibility = View.VISIBLE
+    }
+
+    private fun limparCampos() {
+        binding.campoPeso.text.clear()
+        binding.campoAltura.text.clear()
     }
 }
