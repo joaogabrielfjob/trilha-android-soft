6e6b643ad77ae6d6b3e67bcd7f01dbfe922bbe71
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-01 13:51:18 -0300

    configurando toolbar banco mr

--- banco-mr/app/src/main/java/com/joaogabriel/bancomr/Saldo.kt
+++ banco-mr/app/src/main/java/com/joaogabriel/bancomr/Saldo.kt
@@ -1,11 +1,32 @@
 package com.joaogabriel.bancomr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.joaogabriel.bancomr.databinding.ActivitySaldoBinding
 
 class Saldo : AppCompatActivity() {
+    private lateinit var binding: ActivitySaldoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_saldo)
+
+        binding = ActivitySaldoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarSaldo
+        toolbar.title = "Saldo"
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        toolbar.setTitleMargin(400, 0, 400, 0)
+        toolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.light_blue))
+        toolbar.setNavigationIcon(R.drawable.ic_voltar)
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
