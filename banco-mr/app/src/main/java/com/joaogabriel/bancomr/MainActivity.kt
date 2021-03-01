0321367b8a78f5b8a0cb8d010a8f96b8fcd8bef0
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-01 13:42:33 -0300

    configurando tela saldo app banco mr

--- banco-mr/app/src/main/java/com/joaogabriel/bancomr/MainActivity.kt
+++ banco-mr/app/src/main/java/com/joaogabriel/bancomr/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.joaogabriel.bancomr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joaogabriel.bancomr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saldo.setOnClickListener {
+            val intent = Intent(this, Saldo::class.java)
+            startActivity(intent)
+        }
     }
 }
