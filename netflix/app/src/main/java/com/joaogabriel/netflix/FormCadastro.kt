d8dccac3dcd48385681e1d2f7c7df32b5fcee79a
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-03 15:54:34 -0300

    adicionando firebase ao projeto e cadastrando usuario

--- netflix/app/src/main/java/com/joaogabriel/netflix/FormCadastro.kt
+++ netflix/app/src/main/java/com/joaogabriel/netflix/FormCadastro.kt
@@ -3,12 +3,18 @@ package com.joaogabriel.netflix
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.joaogabriel.netflix.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
     private lateinit var binding: ActivityFormCadastroBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +25,50 @@ class FormCadastro : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        toolbar()
+
+        binding.btCadastrar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagemErro = binding.mensagemErro
+
+            if (email.isEmpty() || senha.isEmpty()) {
+                mensagemErro.text = "Preencha todos os campos"
+            } else {
+                cadastrarUsuario()
+            }
+        }
+    }
+
+    private fun cadastrarUsuario() {
+        val email = binding.editEmail.text.toString()
+        val senha = binding.editSenha.text.toString()
+        val mensagemErro = binding.mensagemErro
+
+        firebaseAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Usuario cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                binding.editEmail.text.clear()
+                binding.editSenha.text.clear()
+                mensagemErro.text = ""
+            }
+        }.addOnFailureListener {
+            var erro = it
+
+            when {
+                erro is FirebaseAuthWeakPasswordException -> mensagemErro.text = "Digite uma senha com no minimo 6 caracteres"
+                erro is FirebaseAuthUserCollisionException -> mensagemErro.text = "Este email ja foi cadastrado"
+                erro is FirebaseNetworkException -> mensagemErro.text = "Sem conexao com a internet"
+
+                else -> mensagemErro.text = "Erro ao cadastrar usuario"
+            }
+        }
+    }
+
+    private fun toolbar() {
         val toolbar = binding.toolbarCadastro
-        toolbar.setBackgroundColor(getColor(R.color.white))
+        toolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
         toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_netflix_official_logo)
     }
 }
