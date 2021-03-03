e2472b1ff4967c7f0fc047fc1295bae0514666f9
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-03 13:56:09 -0300

    adicionando layout da tela de cadastro

--- netflix/app/src/main/java/com/joaogabriel/netflix/FormCadastro.kt
+++ netflix/app/src/main/java/com/joaogabriel/netflix/FormCadastro.kt
@@ -1,11 +1,26 @@
 package com.joaogabriel.netflix
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.joaogabriel.netflix.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+    private lateinit var binding: ActivityFormCadastroBinding
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_cadastro)
+
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarCadastro
+        toolbar.setBackgroundColor(getColor(R.color.white))
+        toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_netflix_official_logo)
     }
 }
