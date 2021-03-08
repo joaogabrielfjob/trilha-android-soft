d4d42a615b58b10ef4df60b5a8d99ca62fe3a567
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-08 18:29:22 -0300

    configurando layouts

---
+++ marvel/app/src/main/java/com/joaogabriel/marvel/CharacterFragment.kt
@@ -0,0 +1,12 @@
+package com.joaogabriel.marvel
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class CharacterFragment : Fragment(R.layout.fragment_character) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ marvel/app/src/main/java/com/joaogabriel/marvel/DetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.joaogabriel.marvel
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment(R.layout.fragment_character_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
