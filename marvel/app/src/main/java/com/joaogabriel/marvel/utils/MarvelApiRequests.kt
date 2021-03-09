1750435b3ca86f9c70a6091b47100c951cbeb02f
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-09 16:31:54 -0300

    adicionando class para gerar hash da marvel e class dos requests a api

---
+++ marvel/app/src/main/java/com/joaogabriel/marvel/utils/MarvelApiRequests.kt
@@ -0,0 +1,14 @@
+package com.joaogabriel.marvel.utils
+
+import com.joaogabriel.marvel.model.MarvelData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MarvelApiRequests {
+    
+    @GET("characters")
+    fun getCharacters(@Query("apikey") apikey: String,
+                      @Query("ts") ts: String,
+                      @Query("hash") hash: String): Call<MarvelData>
+}
