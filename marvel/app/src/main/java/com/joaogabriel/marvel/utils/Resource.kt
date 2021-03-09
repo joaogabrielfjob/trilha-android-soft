e9d172608434263c908a41bfc80c2bbaae1cc08f
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-09 16:29:59 -0300

    adicionando class para estados do resource response

--- marvel/app/src/main/java/com/joaogabriel/marvel/utils/Resource.kt
+++ marvel/app/src/main/java/com/joaogabriel/marvel/utils/Resource.kt
@@ -1,4 +1,12 @@
 package com.joaogabriel.marvel.utils
 
-class Resource {
+import com.joaogabriel.marvel.model.MarvelData
+
+sealed class Resource<T>(
+    val data: MarvelData? = null,
+    val message: String? = null) {
+
+    class Success<T>(data: MarvelData) : Resource<T>(data)
+    class Error<T>(data: MarvelData? = null, message: String?) : Resource<T>(data, message)
+    class Loading<T> : Resource<T>()
 }
