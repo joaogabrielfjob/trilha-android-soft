b2964d800b10859b5786f88a9651b32ce28cd8be
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-09 16:30:33 -0300

    configurando fragments

--- marvel/app/src/main/java/com/joaogabriel/marvel/view/CharacterFragment.kt
+++ marvel/app/src/main/java/com/joaogabriel/marvel/view/CharacterFragment.kt
@@ -3,11 +3,38 @@ package com.joaogabriel.marvel.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.joaogabriel.marvel.R
+import com.joaogabriel.marvel.model.repository.CharacterRepository
+import com.joaogabriel.marvel.utils.Resource
+import com.joaogabriel.marvel.viewmodel.CharacterViewModel
 
 class CharacterFragment : Fragment(R.layout.fragment_character) {
+    private lateinit var characterViewModel: CharacterViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        characterViewModel = ViewModelProvider(this,
+        CharacterViewModel.CharacterViewModelFactory(CharacterRepository())).
+        get(CharacterViewModel::class.java)
+
+        characterViewModel.characterResponse.observe(viewLifecycleOwner, { response ->
+            when(response) {
+                is Resource.Success -> {
+                    println(response.data?.data?.results)
+                }
+
+                is Resource.Error -> {
+                    println(response.message)
+                }
+
+                is Resource.Loading -> {
+                    println("carregando")
+                }
+            }
+        })
+
+        characterViewModel.getCharacters()
     }
 }
--- marvel/app/src/main/java/com/joaogabriel/marvel/view/SplashFragment.kt
+++ marvel/app/src/main/java/com/joaogabriel/marvel/view/SplashFragment.kt
@@ -3,11 +3,14 @@ package com.joaogabriel.marvel.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.joaogabriel.marvel.R
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Navigation.findNavController(requireView()).navigate(R.id.splashToCharacter)
     }
 }
