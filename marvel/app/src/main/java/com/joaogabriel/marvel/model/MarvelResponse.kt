f76272f9bf723ea34dad734688aeb762e281551a
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-10 15:40:37 -0300

    pegando response da imagem do personagem

--- marvel/app/src/main/java/com/joaogabriel/marvel/model/MarvelResponse.kt
+++ marvel/app/src/main/java/com/joaogabriel/marvel/model/MarvelResponse.kt
@@ -6,8 +6,14 @@ data class MarvelData(val data: MarvelResults)
 
 data class MarvelResults(val results: ArrayList<MarvelResult>)
 
-data class MarvelResult(val name: String, val comics: MarvelComics)
+data class MarvelResult(val name: String, val description: String, val comics: MarvelComics, val thumbnail: CharacterImage)
 
 data class MarvelComics(val items: ArrayList<MarvelComic>)
 
 data class MarvelComic(val name: String)
+
+data class CharacterImage(val path: String, val extension: String) {
+    fun getPathExtension(): String {
+        return "$path.$extension"
+    }
+}
