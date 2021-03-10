d63f30d1a6a53d53bbb7f853bad75b9aece49146
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-10 15:41:35 -0300

    ajustes na splash para ela durar 2s na tela

--- marvel/app/src/main/java/com/joaogabriel/marvel/view/SplashFragment.kt
+++ marvel/app/src/main/java/com/joaogabriel/marvel/view/SplashFragment.kt
@@ -1,6 +1,8 @@
 package com.joaogabriel.marvel.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -11,6 +13,8 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Navigation.findNavController(requireView()).navigate(R.id.splashToCharacter)
+        Handler(Looper.getMainLooper()).postDelayed({
+            Navigation.findNavController(requireView()).navigate(R.id.splashToCharacter)
+        }, 2000)
     }
 }
